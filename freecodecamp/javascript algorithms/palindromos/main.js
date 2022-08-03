64aa40e8d848ea31147b1631949518636bfee697
breverdewols <agustinlearn@protonmail.com>
2022-08-03 12:25:13 -0300

    comments imports

--- freecodecamp/javascript algorithms/palindromos/main.js
+++ freecodecamp/javascript algorithms/palindromos/main.js
@@ -1,13 +1,13 @@
 function palindrome(str) {
-    var stringClean = str.toLowerCase().replace(/[\W_]/g, "") //Clean string of 
-    var splitString = stringClean.split('')
-    var reversedString = splitString.reverse()
-    var joinArray = reversedString.join(""); //String
+    var stringClean = str.toLowerCase().replace(/[\W_]/g, "") //Clean string to alphanumeric
+    var splitString = stringClean.split('') //Split string into a array
+    var reversedString = splitString.reverse() //reverse string with method (a alternatived method is use a for)
+    var joinArray = reversedString.join(""); //join array into a string
     if (stringClean == joinArray) {
-        return true 
+        return true                     //check condition started string with final string
     } else {
         return false
     }
 }
 
-palindrome("2_A3*3#A2");
+palindrome("2_A3*3#A2"); //Sample String
